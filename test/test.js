e9a75058696f264b31d7e5fc82c5c11a0d7b6777
AvraamMavridis <avr.mav@gmail.com>
2015-12-11 13:39:55 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,35 +1,9 @@
 // Dependencies
-var validate = require('../validate');
-var chai  = require('chai');
-var expect = chai.expect;
+import { validate } from '../lib/validate';
+import { expect } from 'chai';
 
-options = {
-  url: 'https://www.npmjs.com/',
-  timeout: 60,
-  viewport: '1280x1024',
-  engine: 'webkit',
-  userAgent: 'Chrome/37.0.2062.120',
-  noExternals: false,
-  performanceBudget: {
-    requests: 2,
-    medianLatency: 10,
-    slowestResponse: 1000
-  }
-}
-
-options2 = {
-  someInvalid: 'invalid'
-}
-
-describe('Validation', function(){
-  it('should return true when the options are valid', function(){
-    validation = validate(options);
-    expect(validation).to.be.true;
-  })
-})
-
-describe('Validation', function(){
-  it('should throw an exception when the options are not valid', function(){
-    expect(validate).to.throw(Error)
-  })
-})
+describe('validation tests', function(){
+  it('should have validate method defined',function(){
+      expect( validate ).to.exist;
+    })
+});
