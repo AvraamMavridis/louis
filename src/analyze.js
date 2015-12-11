f5f897e8d8954c9f69316a1f28eeb1ee8b62c5e1
AvraamMavridis <avr.mav@gmail.com>
2015-12-11 12:04:51 +0100

    Code style fixes, move to es6, results file with timestamp

--- src/analyze.js
+++ src/analyze.js
@@ -1,61 +1,114 @@
-// Internal dependecies
-const checkBudget = require( './performanceBudget' );
+/**
+* Constants
+*
+* @author  Avraam Mavridis      <avr.mav@gmail.com>
+*
+*/
 
-
-import { validate } from './validate';
-import * as clc from 'cli-color';
+/** External Dependecies **/
+import { black, yellow } from 'cli-color';
 import * as fs from 'fs';
 import { exec } from 'child_process';
 
-// Build the phantomas command
-// {options} object
-function buildCommand ( options ){
-  validate( options );
+/** Internal Dependecies **/
+import { validate } from './validate';
+import { checkBudget } from './performanceBudget';
 
-  var command = __dirname + '/../node_modules/.bin/phantomas ' + options.url;
+/**
+ * Returns the current Date in dd-mm-yyyy format
+ *
+ * @return { string } Date in dd-mm-yyyy format
+ */
+function getCurrentDate() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  const yyyy = today.getFullYear();
+
+  if ( dd < 10 ) {
+    dd = '0' + dd;
+  }
+  if ( mm < 10 ) {
+    mm ='0'+ mm;
+  }
+
+  today = `${dd}-${mm}-${yyyy}`;
+  return today;
+}
+
+/**
+ * Returns the results filename
+ *
+ * @return { string } filename
+ */
+function getResultsFileName() {
+  return `results-${getCurrentDate()}.json`;
+}
+
+/**
+ * Builds the command than will be passed to phantomas
+ *
+ * @param { object} options
+ * @return { string } command to run on phantomas
+ */
+function buildCommand( options ) {
+  const optionsIsValid = validate( options );
+
+  if ( !optionsIsValid ) {
+    throw new Error( 'Invalid options' );
+  }
+
+  let command = __dirname + '/../node_modules/.bin/phantomas ' + options.url;
 
   command += ' --engine ' + options.engine;
   command += ' --runs ' + options.runs;
-  command += ' --timeout ' +  options.timeout;
+  command += ' --timeout ' + options.timeout;
   command += ' --viewport ' + options.viewport;
 
-  if( options.noExternals )
+  if ( options.noExternals )
+  {
     command += '--no-externals';
+  }
+
+  command += ` --reporter=json > ${getResultsFileName()}`;
 
   return command;
 }
 
-// Exucute and analyze the resuls
-// {options} object
-function _analyze ( options, callback ){
+/**
+ * Analyzes the results and prints them on the console
+ *
+ * @param { object} options
+ */
+function _analyze( options, callback ) {
   const command = buildCommand( options );
 
-  exec( command + ' --reporter=json > results.json', function ( error, stdout, stderr ){
-    if( error !== null ){
+  exec( command, function ( error, stdout, stderr ) {
+    if ( error !== null ) {
       console.log( error );
       return false;
     }
-    else {
-        fs.readFile( 'results.json', function ( error, data ){
 
-          data = JSON.parse( data );
-          const metrics = data.metrics;
+    fs.readFile( getResultsFileName(), function ( err, data ) {
 
-          //if the performanceBudget object is not empty analyze based on that
-          if( Object.keys( options[ 'performanceBudget' ] ).length ){
-            checkBudget( options.performanceBudget, data );
-          }
-          else{
-            var m = Object.keys( metrics );
-            var m_length = m.length;
-            console.log( '\n' + clc.black.bgWhiteBright( 'Metrics analyzed' ) + '\n' );
-            while( m_length-- ){
-              console.log( clc.black.bgYellowBright.underline( m[m_length] ) + ' ' + clc.yellow.bgBlack( metrics[m[m_length]] ) );
-            }
-            callback();
-          }
-        } );
-    }
+      data = JSON.parse( data );
+      const metrics = data.metrics;
+      const m = Object.keys( metrics );
+      let mLength = m.length;
+
+      // If the performanceBudget object is not empty analyze based on that
+      if ( Object.keys( options.performanceBudget ).length ) {
+        checkBudget( options.performanceBudget, data );
+      }
+      else {
+        console.log( '\n' + black.bgWhiteBright( 'Metrics analyzed' ) + '\n' );
+        while ( mLength-- ) {
+          console.log( black.bgYellowBright.underline( m[ mLength ] ) + ' ' + yellow.bgBlack( metrics[ m[ mLength ] ] ) );
+        }
+        callback();
+      }
+    } );
+
   } );
 
 }
